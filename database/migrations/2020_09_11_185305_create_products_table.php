36b891a71433f47b26868c3ceab57bec365c883c
Wesley Flôres <wesleyfloresterres@gmail.com>
2020-09-21 15:00:23 -0300

    migrations

--- database/migrations/2020_09_11_185305_create_products_table.php
+++ database/migrations/2020_09_11_185305_create_products_table.php
@@ -15,17 +15,67 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-
-            $data = file_get_contents(dirname(__DIR__) . DIRECTORY_SEPARATOR . 'seeders' . DIRECTORY_SEPARATOR . 'Product.JSON');
-
-            $o = json_decode($data);
-
-            $fields = array_keys((array)$o[array_key_first($o)]);
-
-            foreach ($fields as $field) {
-                $table->string(strtolower($field), 64)->nullable(false);
-            }
-
+            $table->string("codigo", 32);
+            $table->string("codigo_interno", 32);
+            $table->string("tipo", 32);
+            $table->string("sub_descricao", 32);
+            $table->string("fabricante", 32);
+            $table->string("marca", 32);
+            $table->string("segmento", 32);
+            $table->string("obs", 32);
+            $table->string("obs_complementar", 32);
+            $table->string("ncm", 32);
+            $table->string("minimo", 32);
+            $table->string("maximo", 32);
+            $table->decimal("preco_entrada", 32);
+            $table->decimal("preco_custo", 8, 2);
+            $table->decimal("preco_medio", 8, 2);
+            $table->decimal("preco_venda", 8, 2);
+            $table->decimal("preco_pesquisa", 8, 2);
+            $table->string("markup", 32);
+            $table->string("coeficiente_markup", 32);
+            $table->string("qtd_multipla_compra", 32);
+            $table->string("qtd_multipla_venda", 32);
+            $table->string("qtd_multipla_aplicacao", 32);
+            $table->string("atacado", 32);
+            $table->string("varejo", 32);
+            $table->string("situacao", 32);
+            $table->string("antigo", 32);
+            $table->string("cor", 32);
+            $table->string("lado", 32);
+            $table->string("posicao", 32);
+            $table->string("localizacao", 32);
+            $table->string("extremidade", 32);
+            $table->string("uso", 32);
+            $table->string("imagem", 32);
+            $table->string("gtin", 32);
+            $table->string("gtin_14", 32);
+            $table->string("medida", 32);
+            $table->string("prazo_garantia", 32);
+            $table->string("obs_garantia", 32);
+            $table->string("grupo_item", 32);
+            $table->string("sub_grupo_item", 32);
+            $table->string("fornecedor_referencial", 32);
+            $table->string("un", 32);
+            $table->string("referencia", 32);
+            $table->string("cst_icms", 32);
+            $table->string("preco_promocao", 32);
+            $table->string("cod_importacao_xml", 32);
+            $table->string("base_calculo_icms_ret", 32);
+            $table->string("perc_icms_ret", 32);
+            $table->string("icms_ret", 32);
+            $table->string("icms_substituto", 32);
+            $table->string("pis_cofins", 32);
+            $table->string("chave_nfe", 44);
+            $table->string("ranking_curva_abc_geral_item", 32);
+            $table->string("curva_abc_geral_item", 32);
+            $table->string("valor_curva_abc_geral_item", 32);
+            $table->string("registro_abc_geral_item", 32);
+            $table->string("codigo_curva_abc_fornecedor", 32);
+            $table->string("valor_curva_abc_fornecedor", 32);
+            $table->string("registro_curva_abc_fornecedor", 32);
+            $table->string("uf_curva_abc_fornecedor", 32);
+            $table->softDeletes();
             $table->timestamps();
         });
     }
--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -8,6 +8,9 @@ use Carbon\Carbon;
 
 class ProductSeeder extends Seeder
 {
+    /** @var string */
+    private const URL = "https://raw.githubusercontent.com/MagicalStrangeQuark/JSON/master/JSON/Colors.JSON";
+
     /**
      * Run the database seeds.
      *
@@ -15,23 +18,101 @@ class ProductSeeder extends Seeder
      */
     public function run()
     {
-        $data = file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'Product.JSON');
+        $data = json_decode(file_get_contents(static::URL));
 
-        $o = json_decode($data);
-
-        $fields = array_keys((array)$o[array_key_first($o)]);
-
-        foreach ($o as $t) {
-            $i = [
+        for ($i = 0; $i < 25; $i++) {
+            DB::table('products')->insert([
+                "codigo" => rand(1, 10000),
+                "codigo_interno" => rand(1, 10000),
+                "tipo" => strtoupper(\Illuminate\Support\Str::random(10)),
+                "sub_descricao" => strtoupper(\Illuminate\Support\Str::random(10)),
+                "fabricante" => strtoupper(\Illuminate\Support\Str::random(10)),
+                "marca" => strtoupper(\Illuminate\Support\Str::random(15)),
+                "segmento" => strtoupper(\Illuminate\Support\Str::random(15)),
+                "obs" => strtoupper(\Illuminate\Support\Str::random(10)),
+                "obs_complementar" => strtoupper(\Illuminate\Support\Str::random(10)),
+                "ncm" => "8511.1000",
+                "minimo" => rand(1, 100),
+                "maximo" => rand(1, 100),
+                "preco_entrada" => (rand(1, 100) + (rand(1, 100) / 100)),
+                "preco_custo" => (rand(1, 100) + (rand(1, 100) / 100)),
+                "preco_medio" => (rand(1, 100) + (rand(1, 100) / 100)),
+                "preco_venda" => (rand(1, 100) + (rand(1, 100) / 100)),
+                "preco_pesquisa" => (rand(1, 100) + (rand(1, 100) / 100)),
+                "markup" => "80%",
+                "coeficiente_markup" => 1.80,
+                "qtd_multipla_compra" => 4,
+                "qtd_multipla_venda" => 4,
+                "qtd_multipla_aplicacao" => 2,
+                "atacado" => false,
+                "varejo" => false,
+                "situacao" => "ATIVO",
+                "antigo" => strtoupper(\Illuminate\Support\Str::random(10)),
+                "cor" => $this->randomColor($data),
+                "lado" => "DIREITO",
+                "posicao" => "DIREITO",
+                "localizacao" => strtoupper(\Illuminate\Support\Str::random(10)),
+                "extremidade" => strtoupper(\Illuminate\Support\Str::random(10)),
+                "uso" => strtoupper(\Illuminate\Support\Str::random()),
+                "imagem" => strtoupper(\Illuminate\Support\Str::random(10)),
+                "gtin" => $this->randomNumber(13),
+                "gtin_14" => $this->randomNumber(14),
+                "medida" => strtoupper(\Illuminate\Support\Str::random(2)),
+                "prazo_garantia" => rand(1, 100),
+                "obs_garantia" => strtoupper(\Illuminate\Support\Str::random()),
+                "grupo_item" => strtoupper(\Illuminate\Support\Str::random()),
+                "sub_grupo_item" => strtoupper(\Illuminate\Support\Str::random()),
+                "fornecedor_referencial" => strtoupper(\Illuminate\Support\Str::random()),
+                "un" => strtoupper(\Illuminate\Support\Str::random(3)),
+                "referencia" => strtoupper(\Illuminate\Support\Str::random()),
+                "cst_icms" => (rand(1, 100) + (rand(1, 100) / 100)),
+                "preco_promocao" => (rand(1, 100) + (rand(1, 100) / 100)),
+                "cod_importacao_xml" => rand(1, 1000),
+                "base_calculo_icms_ret" => 0.00,
+                "perc_icms_ret" => 0.00,
+                "icms_ret" => 0.00,
+                "icms_substituto" => 0.00,
+                "pis_cofins" => 0.00,
+                "chave_nfe" => $this->randomNumber(44),
+                "ranking_curva_abc_geral_item" => rand(1, 100),
+                "curva_abc_geral_item" => "B",
+                "valor_curva_abc_geral_item" => (rand(1, 100) + (rand(1, 100) / 100)),
+                "registro_abc_geral_item" => 10.67,
+                "codigo_curva_abc_fornecedor" => rand(1, 100),
+                "valor_curva_abc_fornecedor" => (rand(1, 100) + (rand(1, 100) / 100)),
+                "registro_curva_abc_fornecedor" => rand(1, 100),
+                "uf_curva_abc_fornecedor" => "RS",
                 'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
                 'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
-            ];
-
-            foreach ($fields as $field) {
-                $i[strtolower($field)] = strtoupper(substr($t->{$field}, 0, 64));
-            }
-
-            DB::table('products')->insert($i);
+            ]);
         }
+    }
+
+    /**
+     * randomNumber.
+     * 
+     * @param void
+     * 
+     * @return string
+     */
+    private function randomNumber(int $n): string
+    {
+        return substr(rand(1, 100000000) . rand(1, 100000000) . rand(1, 100000000) . rand(1, 100000000) . rand(1, 100000000) . rand(1, 100000000), 0, $n);
+    }
+
+    /**
+     * randomColor.
+     * 
+     * @param stdClass $o
+     * 
+     * @return string
+     */
+    private function randomColor(\stdClass $o): string
+    {
+        $keys = array_keys((array)$o);
+
+        $rand = $keys[rand(0, count((array) $o) - 1)];
+
+        return !empty($o->{$rand}->cor) ? strtoupper($o->{$rand}->cor) : $this->randomColor($o);
     }
 }
