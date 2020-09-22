bb55b5fccbe09122486d8dac96e9af765fb07376
Wesley Flôres <wesleyfloresterres@gmail.com>
2020-09-22 03:13:42 -0300

    migrations

--- database/migrations/2020_09_15_012738_create_consumers_table.php
+++ database/migrations/2020_09_15_012738_create_consumers_table.php
@@ -15,17 +15,26 @@ class CreateConsumersTable extends Migration
     {
         Schema::create('consumers', function (Blueprint $table) {
             $table->id();
-
-            $data = file_get_contents(dirname(__DIR__) . DIRECTORY_SEPARATOR . 'seeders' . DIRECTORY_SEPARATOR . 'Consumer.JSON');
-
-            $o = json_decode($data);
-
-            $fields = array_keys((array)$o[array_key_first($o)]);
-
-            foreach ($fields as $field) {
-                $table->string(strtolower($field), 64)->nullable(false);
-            }
-
+            $table->string("codigo", 32);
+            $table->string("razao_social_nome", 64);
+            $table->string("imagem", 32);
+            $table->string("fantasia", 32);
+            $table->string("cnpj_cpf", 32);
+            $table->string("ie", 32);
+            $table->string("endereco", 32);
+            $table->string("num", 32);
+            $table->string("bairro", 32);
+            $table->string("cidade", 32);
+            $table->string("uf", 32);
+            $table->string("telefone", 32);
+            $table->string("cep", 32);
+            $table->string("primeira_venda", 32);
+            $table->string("ultima_venda", 32);
+            $table->string("limite_credito", 32);
+            $table->string("grupo_economico", 32);
+            $table->string("vendedor_interno", 32)->unique();
+            $table->string("vendedor_externo", 32)->unique();
+            $table->string("email", 64);
             $table->timestamps();
         });
     }
